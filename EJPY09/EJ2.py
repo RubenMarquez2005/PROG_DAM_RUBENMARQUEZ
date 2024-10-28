ca75d42eeec02bedefd972c37e3f54790340df90
Rubén Márquez Pérez <ruben.marquez@campusfp.es>
2024-10-28 11:06:17 +0100

    EJERCICIOS NUMPY BASICOS

---
+++ EJPY09/EJ2.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+array1= np.array([3,6,9,8,7,2])
+producto= array1 * 8
+
+print(producto)
---
+++ EJPY09/EJ5.py
@@ -0,0 +1,4 @@
+import numpy as np
+array1= np.array([3,6,9,8,7,2])
+producto= array1 ** 2
+print(producto)
