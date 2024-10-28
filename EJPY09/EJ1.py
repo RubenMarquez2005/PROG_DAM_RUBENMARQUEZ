9517fab4272b0bc71c28cc96778f3548790f0d07
Rubén Márquez Pérez <ruben.marquez@campusfp.es>
2024-10-28 11:08:48 +0100

    EJERCICIOS NUMPY BASICOS 09

---
+++ EJPY09/EJ1.py
@@ -0,0 +1,6 @@
+import numpy as np
+array1 = np.array([15,25,69,72,31])
+array2 = np.array([45,63,25,54,71])
+total = array1 + array2
+
+print(total)
---
+++ EJPY09/EJ3.py
@@ -0,0 +1,3 @@
+import numpy as np
+array = np.array([6,9,8,7,9,7,9,7,10,9])
+print(np.mean(array))
