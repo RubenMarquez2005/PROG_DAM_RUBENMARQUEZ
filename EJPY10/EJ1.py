fff9123328f20dfddbde7f0cc501494252ef0531
Rubén Márquez Pérez <ruben.marquez@campusfp.es>
2024-11-07 14:01:01 +0100

    Delete EJPY10 directory

--- EJPY10/EJ1.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-matriz = np.zeros((3, 3))
-matriz[1, 1] = 1
-print(matriz)
--- EJPY10/EJ3.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-matriz = np.arange(1, 17).reshape((4, 4))
-
-tercera_columna = matriz[:, 2]
-
-print("La tercera columna de la matriz es:")
-print(tercera_columna)
